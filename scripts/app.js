5298de827cbea0e4bece9dc1f31b4627f8b68132
Sergio Hidalgo <shidalgoc@auna.pe>
2024-09-02 22:05:51 -0500

    initial

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+function add() {
+  console.log("press button click");
+}
+
+function add1() {
+  console.log("press button click1");
+}
+
+addBtn.addEventListener("click", add);
+addBtn.addEventListener("click", add1);
+
+//addBtn.removeEventListener("click", add);
---
+++ scripts/vendor.js
@@ -0,0 +1,5 @@
+const userInput = document.getElementById("input-number");
+const addBtn = document.getElementById("btn-add");
+const substractBtn = document.getElementById("btn-substract");
+const multiplyBtn = document.getElementById("btn-multiply");
+const divideBtn = document.getElementById("btn-divide");
