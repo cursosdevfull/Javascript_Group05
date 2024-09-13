7f565aeff2e5798beedbba2b208f7cea3de6bb8d
Sergio Hidalgo <shidalgoc@auna.pe>
2024-09-12 21:51:08 -0500

    monster completed

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,12 +2,33 @@ const MODE_ATTACK = "ATTACK";
 const MODE_STRONG_ATTACK = "STRONG_ATTACK";
 
 const ATTACK_VALUE = 14;
-const STRONG_ATTACK_VALUE = 100;
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 15;
+
+const MAX_LIFE = 100;
+const MAX_AVAILABLE_LIFE = 3;
+const LIFE_AVAILABLE = 3;
+const HEALT_VALUE = 20;
+
+const LOG_EVENT_PLAYER_ATTACK = "PLAYER_ATTACK";
+const LOG_EVENT_PLAYER_STRONG_ATTACK = "PLAYER_STRONG_ATTACK";
+const LOG_EVENT_MONSTER_ATTACK = "MONSTER_ATTACK";
+const LOG_EVENT_PLAYER_HEALTH = "PLAYER_HEALTH";
+const LOG_EVENT_GAME_OVER = "GAME_OVER";
+
+let currentMonsterHealth = MAX_LIFE;
+let currentPlayerHealth = MAX_LIFE;
+let hasBonusLife = true;
+let availableLifes = LIFE_AVAILABLE;
+
+const battleLog = [];
 
 function attackMounsterHandler(mode) {
   const maxDamage = mode == MODE_ATTACK ? ATTACK_VALUE : STRONG_ATTACK_VALUE;
-  console.log("maxDamage", maxDamage);
-  //const mounstroDamage = dealMonsterDamage()
+  const monsterDamage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= monsterDamage;
+
+  endRound();
 }
 
 function attackHandler() {
@@ -18,11 +39,103 @@ function strongAttackHandler() {
   attackMounsterHandler(MODE_STRONG_ATTACK);
 }
 
-function healPlayerHandler() {}
+function endRound() {
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
 
-function printLogHandler() {}
+  const playerWinner = currentMonsterHealth <= 0 && currentPlayerHealth > 0;
+  const monsterWinner = currentPlayerHealth <= 0 && currentMonsterHealth > 0;
+  const draw = currentMonsterHealth <= 0 && currentPlayerHealth <= 0;
 
-attackBtn.addEventListener("click", attackHandler);
+  if (playerWinner) {
+    alert("Player won");
+    writeToLog(LOG_EVENT_GAME_OVER);
+  } else if (monsterWinner) {
+    alert("Monster won");
+    const playerWinner = currentMonsterHealth <= 0 && currentPlayerHealth > 0;
+    const monsterWinner = currentPlayerHealth <= 0 && currentMonsterHealth > 0;
+    const draw = currentMonsterHealth <= 0 && currentPlayerHealth <= 0;
+  } else if (draw) {
+    alert("Draw");
+  }
+
+  if (playerWinner || monsterWinner || draw) {
+    reset();
+  }
+
+  writeToLog(
+    LOG_EVENT_MONSTER_ATTACK,
+    playerDamage,
+    currentMonsterHealth,
+    currentPlayerHealth
+  );
+}
+
+function reset() {
+  currentMonsterHealth = MAX_LIFE;
+  currentPlayerHealth = MAX_LIFE;
+  availableLifes = LIFE_AVAILABLE;
+  hasBonusLife = true;
+
+  resetGame(MAX_LIFE);
+}
+
+function healPlayerHandler() {
+  if (currentMonsterHealth <= 0 || currentPlayerHealth <= 0 || !hasBonusLife)
+    return;
+
+  if (availableLifes > 0) {
+    availableLifes--;
+    updateBonusLife(availableLifes);
+  }
+
+  if (availableLifes <= 0) {
+    hasBonusLife = false;
+    removeBonusLife();
+  }
+
+  let healthValue;
+  if (currentPlayerHealth >= MAX_LIFE - HEALT_VALUE) {
+    healthValue = MAX_LIFE - currentPlayerHealth;
+  } else {
+    healthValue = HEALT_VALUE;
+  }
+
+  currentPlayerHealth += healthValue;
+
+  incrementPlayerHealth(healthValue);
+}
+
+function writeToLog(event, value, monsterHealth, playerHealth) {
+  let logEntry = {
+    event,
+    value,
+    monsterHealth,
+    playerHealth,
+  };
+
+  switch (event) {
+    case LOG_EVENT_PLAYER_ATTACK:
+    case LOG_EVENT_PLAYER_STRONG_ATTACK:
+      logEntry.target = "MONSTER";
+      break;
+    case LOG_EVENT_PLAYER_HEALTH:
+    case LOG_EVENT_PLAYER_HEALTH:
+      logEntry.target = "PLAYER";
+      break;
+    default:
+      logEntry = {};
+
+      battleLog.push(logEntry);
+  }
+}
+
+function printLogHandler() {
+  console.table(battleLog);
+}
+
+attackBtn.addEventListener("click", attackHandler.bind(attackHandler));
 strongAttackBtn.addEventListener("click", strongAttackHandler);
 healBtn.addEventListener("click", healPlayerHandler);
 logBtn.addEventListener("click", printLogHandler);
+updateBonusLife(LIFE_AVAILABLE);
--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -3,6 +3,37 @@ const strongAttackBtn = document.getElementById("strong-attack-btn");
 const healBtn = document.getElementById("heal-btn");
 const logBtn = document.getElementById("log-btn");
 
-const mounsterHealthBar = document.getElementById("strong-app-btn");
+const mounsterHealthBar = document.getElementById("monster-health");
 const playerHealthBar = document.getElementById("player-health");
 const bonusLifeEl = document.getElementById("bonus-life");
+
+function dealMonsterDamage(maxDamage) {
+  const dealDamage = Math.random() * maxDamage;
+  mounsterHealthBar.value = +mounsterHealthBar.value - dealDamage;
+  return dealDamage;
+  // return Math.random()*maxDamage
+}
+
+function dealPlayerDamage(maxDamage) {
+  const dealDamage = Math.random() * maxDamage;
+  playerHealthBar.value = +playerHealthBar.value - dealDamage;
+  return dealDamage;
+}
+
+function resetGame(value) {
+  mounsterHealthBar.value = value;
+  playerHealthBar.value = value;
+}
+
+function updateBonusLife(lifes) {
+  if (lifes <= 0) return;
+  bonusLifeEl.textContent = lifes;
+}
+
+function removeBonusLife() {
+  bonusLifeEl.parentNode.removeChild(bonusLifeEl);
+}
+
+function incrementPlayerHealth(value) {
+  playerHealthBar.value += value;
+}
